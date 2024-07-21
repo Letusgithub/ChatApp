eda2f8401e52e68c54dcd987e256cdcd88471ea4
Aditya <anandaditya049@gmail.com>
2024-07-21 16:52:54 +0530

    Added Env File

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
 import io from 'socket.io-client';
-import { useState } from "react";
+import {  useState } from "react";
 import Chat from './Chat';
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect(process.env.REACT_APP_BACKEND);
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
 
   const [showChat, setShowChat] = useState(false);
-
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
